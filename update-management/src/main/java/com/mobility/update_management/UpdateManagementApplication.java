d53f6bbe96473cc7ceb7120321b600c7dada3886
Aryan Avnish <aryan.avnish02@gmail.com>
2025-09-12 22:20:19 +0530

    Added services for campaign, vehicle, firmware and update management (#1)
    Co-authored-by: aryanavnish <aryan.avnish@valtech.com>

---
+++ update-management/src/main/java/com/mobility/update_management/UpdateManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mobility.update_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateManagementApplication.class, args);
+	}
+
+}
---
+++ update-management/src/test/java/com/mobility/update_management/UpdateManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobility.update_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
