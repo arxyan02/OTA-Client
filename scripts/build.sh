57f6102b9570b66707963d31d31514e8549e50b7
aryanavnish <aryan.avnish@valtech.com>
2025-09-23 13:19:26 +0530

    Added workflow to build and publish packages on PR completion

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+cd .. && cd campaign-management && jenv exec mvn clean install && \
+cd ../firmware-management && jenv exec mvn clean install && \
+cd ../update-management && jenv exec mvn clean install && \
+cd ../vehicle-management && jenv exec mvn clean install
