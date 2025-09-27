fa7cae664ff4f574a8d0c36fd6a2fd5bac09c9fa
aryanavnish <aryan.avnish@valtech.com>
2025-09-27 13:59:54 +0530

    Added terraform provisioning and configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,125 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "valtech-ota"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "valtech-ota-cluster"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.27"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "node_instance_types" {
+  description = "EC2 instance types for EKS nodes"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "min_nodes" {
+  description = "Minimum number of nodes"
+  type        = number
+  default     = 1
+}
+
+variable "max_nodes" {
+  description = "Maximum number of nodes"
+  type        = number
+  default     = 10
+}
+
+variable "desired_nodes" {
+  description = "Desired number of nodes"
+  type        = number
+  default     = 3
+}
+
+variable "service_names" {
+  description = "List of Spring Boot service names"
+  type        = list(string)
+  default     = [
+    "campaign-management",
+    "firmware-management",
+    "update-management",
+    "vehicle-management"
+  ]
+}
+
+# Database variables
+variable "create_rds" {
+  description = "Whether to create RDS instance"
+  type        = bool
+  default     = true
+}
+
+variable "db_allocated_storage" {
+  description = "Initial allocated storage for RDS"
+  type        = number
+  default     = 20
+}
+
+variable "db_max_allocated_storage" {
+  description = "Maximum allocated storage for RDS"
+  type        = number
+  default     = 100
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  default     = "valtech_ota"
+}
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+  default     = "postgres"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
